66289851b1af903130ef95d607bc2e8b1c695bc9
Victor Yanson <victor.yanson@outlook.com>
2026-06-02 08:43:08 +0000

    fix: heap allocate sample times

--- src/benchmarks/astar_bench.cc
+++ src/benchmarks/astar_bench.cc
@@ -19,32 +19,26 @@ void astar_bench::compute() {
   Astar router;
   Graph const bench_graph = createMockGraph(MockGraphType::TRIANGLE);
   steady_clock_timer timer;
-  uint64_t samples[sample_size];
+  std::vector<uint64_t> samples(sample_size);
 
   for (int i = 0; i < sample_size; i++) {
+    // start timer
+    timer.restart();
     // run algorithm
     Path route = router.calculatePath(bench_graph, 0, 2);
     // record time
     samples[i] = timer.nanoSeconds();
   }
 
-  uint64_t min_time = *std::min_element(samples, samples + sample_size);
-  uint64_t max_time = *std::max_element(samples, samples + sample_size);
+  uint64_t min_time = *std::min_element(samples.begin(), samples.end());
+  uint64_t max_time = *std::max_element(samples.begin(), samples.end());
 
-  double total_time = std::accumulate(samples, samples + sample_size, 0.0);
+  double total_time = std::accumulate(samples.begin(), samples.end(), 0.0);
   double average_time = total_time / sample_size;
 
   uart::write("Bench finished.\n");
 
-  uart::write("Min execution time: ");
-  uart::write(std::to_string(min_time));
-  uart::write(" ns\n");
-
-  uart::write("Max execution time: ");
-  uart::write(std::to_string(max_time));
-  uart::write(" ns\n");
-
-  uart::write("Average execution time: ");
-  uart::write(std::to_string(average_time));
-  uart::write(" ns\n");
+  printf("Min execution time: %llu ns\n", (unsigned long long)min_time);
+  printf("Max execution time: %llu ns\n", (unsigned long long)max_time);
+  printf("Average execution time: %.2f ns\n", average_time);
 }
