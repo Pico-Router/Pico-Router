2a2a878d3b677db39cf2829292442f18c8e49cc7
Julis computer <juliana.jgm63@gmail.com>
2026-06-02 13:41:05 +0000

    fix: include system_init header in platfrom implementation

--- platform/host/system_init_host.cc
+++ platform/host/system_init_host.cc
@@ -1,3 +1,5 @@
 #include <iostream>
 
+#include "platform/system_init.h"
+
 void platform_init() { std::cout << "Initialized.\n"; }
--- platform/pico/system_init_pico.cc
+++ platform/pico/system_init_pico.cc
@@ -1,3 +1,4 @@
 #include "pico/stdlib.h"
+#include "platform/system_init.h"
 
 void platform_init() { stdio_init_all(); }
