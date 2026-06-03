3bdbc4e8b70c9aef52009cc61acc799a858c511a
Victor Yanson <victor.yanson@outlook.com>
2026-06-03 12:44:33 +0000

    fix: add platform_sleep_ms() for host

--- platform/host/system_init_host.cc
+++ platform/host/system_init_host.cc
@@ -1,7 +1,13 @@
+#include <chrono>
 #include <iostream>
+#include <thread>
 
 #include "platform/system_init.h"
 
 pathfind::Astar System::router_;
 
 void platform_init() { std::cout << "Initialized.\n"; }
+
+void platform_sleep_ms(uint32_t ms) {
+  std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+}
