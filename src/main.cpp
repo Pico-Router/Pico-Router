f1ef3c1548ef7c4b26279275b68c4d0dd39d8f76
Victor Yanson <victor.yanson@outlook.com>
2026-08-31 19:55:56 +0000

    chore: update makefile

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,11 @@
-#include "benchmarks/astar_bench.hpp"
-#include "demo/astar_demo.hpp"
 #include "platform/system_init.hpp"
 #include "platform/uart.hpp"
 
 int main() {
   platform_init();
   System global_system;
-  demo::astar_demo main_demo;
 
   // await_input();
-  main_demo.run(global_system);
 
   // while (true) {
   //   platform_sleep_ms(1000);
