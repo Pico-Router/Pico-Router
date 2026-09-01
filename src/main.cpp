59008ccbdf3f3e20d54f1632d5fa415886dc7bc7
Victor Yanson <victor.yanson@outlook.com>
2026-09-01 15:06:31 +0000

    ci: set PICO_SDK_PATH for pico firmware build

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,11 @@
 
 int main() {
   platform_init();
-  System global_system;
 
-  // await_input();
+  while (true) {
+    platform_sleep_ms(1000);
+    uart::write("Hello World!\n");
+  };
 
-  // while (true) {
-  //   platform_sleep_ms(1000);
-  //   uart::write("Hello World!\n");
-  // };
   return 0;
 }
